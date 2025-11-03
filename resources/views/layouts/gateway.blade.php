48036444db458f563c3e5e70aa6518f2aeacf5ab
Sten Seegel <sten.seegel@gmail.com>
2025-11-03 12:05:07 +0100

    feat: update title references to use config('app.name') for consistency

--- resources/views/layouts/gateway.blade.php
+++ resources/views/layouts/gateway.blade.php
@@ -6,7 +6,7 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 
-    <title>{{ env('APP_NAME') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <link rel="stylesheet" href="{{ asset('css_v2.1.0/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css_v2.1.0/handshake_style.css') }}">
